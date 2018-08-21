4fe6da7ca228d5cb515f9df1bf0128e6effe43f4
subhashreddy22 <subhashreddy22@gmail.com>
2018-08-21 10:58:27 +0530

    added code in service method

--- first-servlet/src/com/servlet/FirstServlet.java
+++ first-servlet/src/com/servlet/FirstServlet.java
@@ -1,6 +1,8 @@
 package com.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -8,7 +10,8 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 
-@WebServlet(value = "/welcome") // This can be used instead of servlet-mapping in web.xml
+@WebServlet(value = "/welcome") // This can be used instead of servlet-mapping
+								// in web.xml
 public class FirstServlet implements Servlet {
 
 	@Override
@@ -33,15 +36,17 @@ public class FirstServlet implements Servlet {
 	public void init(ServletConfig arg0) throws ServletException {
 		// TODO Auto-generated method stub
 		System.out.println("Inside init");
-		
+
 	}
 
 	@Override
-	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException {
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("Inside service");
-		
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		pw.print("<html><body>");
+		pw.print("<b> Welcome </b>");
+		pw.print("</body></html>");
 	}
-
 
 }
